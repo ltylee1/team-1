9f824c46807de1d15ee98ca962fc606059bd824a
Shakthi Wijeratne <shakthi@dhcp-206-12-71-99.ubcsecure.wireless.ubc.ca>
2017-02-11 13:54:40 -0800

    using mysql instead of sqlite3

--- backend/settings.py
+++ backend/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unitedWay',
+        'USER': 'unitedWay',
+        'PASSWORD': 'Etfhg8893',
+        'HOST': 'uw-test.cromnho1ocid.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
