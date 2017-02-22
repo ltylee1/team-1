52c64f547f8ffeb0f106bf2762f45e2160308745
shakthi124 <shakthiwije@gmail.com>
2017-02-22 15:50:02 -0800

    Update urls.py

--- unitedairway/urls.py
+++ unitedairway/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('uw-dashboard.urls')),
+    url(r'^', include('uw_dashboard.urls')),
 ]
