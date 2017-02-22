394cb6194aea669305d798d0a8aeeaeb693e5a37
shakthi124 <shakthiwije@gmail.com>
2017-02-22 14:23:11 -0800

    Update settings.py

--- unitedairway/unitedairway/settings.py
+++ unitedairway/unitedairway/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'unitedairway.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unitedWayTest',
+        'USER': 'unitedWay',
+        'PASSWORD': 'Etfhg8893',
+        'HOST': 'uw-test.cromnho1ocid.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
