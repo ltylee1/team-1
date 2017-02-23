afa1689e342dbe2a77766e185923817481594e74
Ty <lulu.tylee@gmail.com>
2017-02-22 23:09:34 -0800

    Updated database models and migrations

--- uw_dashboard/models.py
+++ uw_dashboard/models.py
@@ -6,42 +6,49 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Agencies(models.Model):
-    agency_andar_number = models.IntegerField(default=0)
+    agency_andar_number = models.IntegerField(default=0, primary_key=True)
     agency_name = models.CharField(max_length=128)
+
 
 class Program(models.Model):
     agency_andar_number = models.ForeignKey(Agencies, on_delete=models.CASCADE)
-    program_andar_number = models.IntegerField(default=0)
+    program_andar_number = models.IntegerField(default=0, primary_key=True)
     program_name = models.CharField(max_length=128)
     grant_start_date = models.DateField()
     grant_end_date = models.DateField()
-    program_discription = models.CharField(max_length=128)
+    program_description = models.CharField(max_length=128)
     program_planner = models.CharField(max_length=128)
     funds = models.CharField(max_length=128)
     focus_area = models.CharField(max_length=128)
     strategic_outcome = models.CharField(max_length=128)
     funding_stream = models.CharField(max_length=128)
-    allocation = models.IntegerField(default=0)
+    allocation = models.FloatField(default=0)
     year = models.IntegerField(default=2016)
     website = models.CharField(max_length=128)
 
+
 class Target_Population(models.Model):
     program_andar_number = models.ForeignKey(Program, on_delete=models.CASCADE)
-    target_Population = models.CharField(max_length=128)
+    target_population = models.CharField(max_length=128)
+
 
 class Geo_Focus_Area(models.Model):
     program_andar_number = models.ForeignKey(Program, on_delete=models.CASCADE)
-    city = models.CharField(max_length=128)
+    city = models.CharField(max_length=150)
     percent_of_focus = models.IntegerField(default=0)
 
-class Doner_Engagement(models.Model):
+
+class Donor_Engagement(models.Model):
     program_andar_number = models.ForeignKey(Program, on_delete=models.CASCADE)
-    doner_engagement = models.CharField(max_length=128)
+    donor_engagement = models.CharField(max_length=128)
+
 
 class Totals(models.Model):
     program_andar_number = models.ForeignKey(Program, on_delete=models.CASCADE)
+    total_clients = models.IntegerField(default=0)
     early_years = models.IntegerField(default=0)
     middle_years = models.IntegerField(default=0)
+    children = models.IntegerField(default=0)
     seniors = models.IntegerField(default=0)
     parent_caregivers = models.IntegerField(default=0)
     families = models.IntegerField(default=0)
@@ -52,16 +59,19 @@ class Totals(models.Model):
     workshops = models.IntegerField(default=0)
     volunteers = models.IntegerField(default=0)
 
+
 class Program_Elements(models.Model):
     program_andar_number = models.ForeignKey(Program, on_delete=models.CASCADE)
     level = models.IntegerField(default=0)
-    element_name = models.IntegerField(default=0)
-    specific_element = models.IntegerField(default=0)
+    element_name = models.CharField(max_length=128)
+    specific_element = models.CharField(max_length=128)
+
 
 class Location(models.Model):
-	program_andar_number = models.ForeignKey(Program, on_delete=models.CASCADE)
-	location = models.CharField(max_length=128)
-	postal_code = models.CharField(max_length=128)
+    program_andar_number = models.ForeignKey(Program, on_delete=models.CASCADE)
+    location = models.CharField(max_length=128)
+    postal_code = models.CharField(max_length=128)
+
 
 # Separate table for extra user information we need that is not used for authentication
 class Profile(models.Model):
