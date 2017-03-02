5d75b2529a04cb995e0ebac537c4cce3ff44db93
Richard Chiang <shadowlairs@gmail.com>
2017-03-01 16:22:24 -0800

    Add html views to addUser, login, and upload link

--- uw_dashboard/urls.py
+++ uw_dashboard/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 
-from .views import Homepage
+from .views import Homepage, UploadView, LoginView, AddUserView, MapView
 
 urlpatterns = [
     url(r'^$', Homepage.as_view(), name='index'),
+    url(r'^upload.html$', UploadView.as_view(), name="upload"),
+    url(r'^login.html$', LoginView.as_view(), name="login"),
+    url(r'^addUser.html$', AddUserView.as_view(), name="addUser"),
+    url(r'^map.html$', MapView.as_view(), name="map")
 ]
--- uw_dashboard/views.py
+++ uw_dashboard/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
-from django.http import HttpResponse
-
 class Homepage(TemplateView):
+    template_name = "homepage.html"
 
-    def get_template_names(self):
-        return "homepage.html"
 
+class UploadView(TemplateView):
+    template_name = "upload.html"
+
+class LoginView(TemplateView):
+    template_name = "login.html"
+
+class MapView(TemplateView):
+    template_name = "map.html"
+
+class AddUserView(TemplateView):
+    template_name = "addUser.html"
