24f422f28fd223e8c4b06b261afa8101ad39e294
Richard Chiang <shadowlairs@gmail.com>
2017-03-04 14:27:43 -0800

    Add Login required for views

--- uw_dashboard/views.py
+++ uw_dashboard/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView, RedirectView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.core.urlresolvers import reverse_lazy
 
-class Homepage(TemplateView):
+class Homepage(LoginRequiredMixin, TemplateView):
     template_name = "homepage.html"
 
-class UploadView(TemplateView):
+class UploadView(LoginRequiredMixin, TemplateView):
     template_name = "upload.html"
 
-class LoginView(TemplateView):
+class LoginView(FormView):
     template_name = "login.html"
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('homepage')
 
-class MapView(TemplateView):
+    def form_valid(self, form):
+        user = form.get_user()
+        login(self.request, user)
+        return super(LoginView, self).form_valid(form)
+
+
+class LogoutView(RedirectView):
+    url = reverse_lazy('login')
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
+
+class MapView(LoginRequiredMixin, TemplateView):
     template_name = "map.html"
 
-class AddUserView(TemplateView):
+class AddUserView(LoginRequiredMixin, TemplateView):
     template_name = "addUser.html"
 
-class SearchResultsView(TemplateView):
+class SearchResultsView(LoginRequiredMixin, TemplateView):
     template_name = "search-results.html"
