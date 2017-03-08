9e4263a3b38636738ace05765366f5428d4742f9
Vivek Chachcha <vivek@seekmi.com>
2017-03-07 17:00:33 -0800

    Adding checks for filters

--- uw_dashboard/databaseReader.py
+++ uw_dashboard/databaseReader.py
@@ -16,57 +16,66 @@ class DatabaseReader(models.Model):
 		self.filters = filters
 
 	def readData():
+		filters = self.filters
 		query = "SELECT * FROM Program AS p, Program_Elements AS pe, Target_Population AS t, Geo_Focus_Area AS gfa, Donor_Engagement AS de WHERE"
 
 		joinTables = " p.program_andar_number = pe.program_andar_number AND p.program_andar_number = t.program_andar_number AND p.program_andar_number = gfa.program_andar_number AND p.program_andar_number = de.program_andar_number AND"
 
-		filterYear = " ("
-		for i in range(len(filters['year'])):
-			filterYear += " p.grant_start_date BETWEEN '" + str(filters['year'][i]) + "-01-01' AND '" + str(filters['year'][i]) + "'-12-31' OR"
-		filterYear = filterYear[:-2]
-		filterYear += " AND"
+		if 'year' in filters.keys():
+			filterYear = " ("
+			for i in range(len(filters['year'])):
+				filterYear += " p.grant_start_date BETWEEN '" + str(filters['year'][i]) + "-01-01' AND '" + str(filters['year'][i]) + "'-12-31' OR"
+			filterYear = filterYear[:-2]
+			filterYear += " AND"
+	
+		if 'focus_area' in filters.keys():
+			focusArea = " ("
+			for i in range(len(filters['focus_area'])):
+				focusArea += " p.focus_area = '" + str(filters['focus_area'][i]) + "' OR"
+			focusArea = focusArea[:-2]
+			focusArea += " ) AND"
 
-		focusArea = " ("
-		for i in range(len(filters['focus_area'])):
-			focusArea += " p.focus_area = '" + str(filters['focus_area'][i]) + "' OR"
-		focusArea = focusArea[:-2]
-		focusArea += " ) AND"
+		if 'target_population' in filters.keys():
+			targetPop = " ("
+			for i in range(len(filters['target_population'])):
+				targetPop += " t.target_population = '" + str(filters['target_population'][i]) + "' OR"
+			targetPop = targetPop[:-2]
+			targetPop += ") AND"
 
-		targetPop = " ("
-		for i in range(len(filters['target_population'])):
-			targetPop += " t.target_population = '" + str(filters['target_population'][i]) + "' OR"
-		targetPop = targetPop[:-2]
-		targetPop += ") AND"
+		if 'program_elements' in filters.keys():
+			programElements = " ("
+			for i in range(len(filters['program_elements'])):
+				programElements += " pe.specific_element = '" + str(filters['program_elements'][i]) + "' OR"
+			programElements = programElements[:-2]
+			programElements += ") AND"
 
-		programElements = " ("
-		for i in range(len(filters['program_elements'])):
-			programElements += " pe.specific_element = '" + str(filters['program_elements'][i]) + "' OR"
-		programElements = programElements[:-2]
-		programElements += ") AND"
+		if 'gfa' in filters.keys():
+			gfa = " ("
+			for i in range(len(filters['gfa'])):
+				gfa += " gfa.city = '" + str(filters['gfa'][i]) + "' OR"
+			gfa = gfa[:-2]
+			gfa += ") AND"
 
-		gfa = " ("
-		for i in range(len(filters['gfa'])):
-			gfa += " gfa.city = '" + str(filters['gfa'][i]) + "' OR"
-		gfa = gfa[:-2]
-		gfa += ") AND"
+		if 'city' in filters.keys():
+			cityGrouping = " ("
+			for i in range(len(filters['city'])):
+	                        cityGrouping += " gfa.city_grouping = '" + str(filters['city'][i]) + "' OR"
+	                cityGrouping = cityGrouping[:-2]
+	                cityGrouping += ") AND"	
+	
+		if 'donor' in filters.keys():
+			donorEngagement = " ("
+			for i in range(len(filters['donor'])):
+				donorEngagement += " de.donor_engagement = '" + str(filters['donor'][i]) + "' OR"
+			donorEngagement = donorEngagement[:-2]
+			donorEngagement += ") AND"
 
-		cityGrouping = " ("
-                for i in range(len(filters['city'])):
-                        cityGrouping += " gfa.city_grouping = '" + str(filters['city'][i]) + "' OR"
-                cityGrouping = cityGrouping[:-2]
-                cityGrouping += ") AND"	
-
-		donorEngagement = " ("
-		for i in range(len(filters['donor'])):
-			donorEngagement += " de.donor_engagement = '" + str(filters['donor'][i]) + "' OR"
-		donorEngagement = donorEngagement[:-2]
-		donorEngagement += ") AND"
-
-		investment = " ("
-		for i in range(len(filters['invested'])):
-			investment += " p.funds = '" + str(filters['invested'][i]) + "' OR"
-		investment = investment[:-2]
-		investment += ")"
+		if 'invested' in filters.keys():
+			investment = " ("
+			for i in range(len(filters['invested'])):
+				investment += " p.funds = '" + str(filters['invested'][i]) + "' OR"
+			investment = investment[:-2]
+			investment += ")"
 
 		finalQuery = query + joinTables + filterYear + focusArea + targetPop + programElements + gfa + cityGrouping + donorEngagement + investment
 
