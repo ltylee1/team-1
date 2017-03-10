4226b8991502a2be9a3f6fd29661b34e142c161a
Vivek Chachcha <vivek@seekmi.com>
2017-03-09 16:30:21 -0800

    queryies

--- uw_dashboard/databaseReader.py
+++ uw_dashboard/databaseReader.py
@@ -97,4 +97,4 @@ class DatabaseReader(models.Model):
 
 		tResults = my_custom_sql(tQuery)
 
-		return {'results': firstResults, 'totals': tResults}
+		return {'results': firstResults, 'totals': tResults, 'query': query, 'tquery': tQuery}
