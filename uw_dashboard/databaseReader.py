93973e546b24118801a0bce8c2cd8348ecab5058
Vivek Chachcha <vivek@seekmi.com>
2017-03-12 14:55:30 -0700

    getting rid of duplicate program numbers

--- uw_dashboard/databaseReader.py
+++ uw_dashboard/databaseReader.py
@@ -86,6 +86,7 @@ class DatabaseReader(models.Model):
 		firstResults = my_custom_sql(query)
 		
 		programsReturned = [i['program_andar_number'] for i in firstResults]
+		programsReturned = list(set(programsReturned))
 
 		tQuery = "SELECT SUM(p.funds) AS invested, COUNT(p.program_andar_number) AS programs, COUNT(DISTINCT p.agency_andar_number_id) AS agencies, SUM(t.early_years) AS early_years, SUM(t.middle_years) AS middle_years, SUM(t.seniors) AS seniors, SUM(t.parent_caregivers) AS parent_caregivers, SUM(t.families) AS families, SUM(t.meals_snacks) as meals_snacks, SUM(t.counselling_sessions) AS counselling_sessions, SUM(t.mentors_tutors) AS mentors_tutors, SUM(t.workshops) as workshops, SUM(t.volunteers) AS volunteers FROM uw_dashboard_program as p, uw_dashboard_totals as t WHERE"
 
