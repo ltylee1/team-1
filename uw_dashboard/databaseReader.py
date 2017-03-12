6ddfdc1d8de6be2cfbc25e4d328f2d7605f5f007
Vivek Chachcha <vivek@seekmi.com>
2017-03-12 14:27:38 -0700

    adding self to parameters

--- uw_dashboard/databaseReader.py
+++ uw_dashboard/databaseReader.py
@@ -1,20 +1,24 @@
 from django.db import connection, models
 
-def my_custom_sql(query):
-	with connection.cursor() as cursor:
-		cursor.execute(query)
-		results = cursor.fetchall()
-		columns = [col[0] for col in cursor.description]
-	return [
-		dict(zip(columns, row))
-		for row in cursor.fetchall()
-	]
-
 class DatabaseReader(models.Model):
 
 	def __init__(self, filters):
 		self.filters = filters
 
+	def my_custom_sql(self, query):
+	        with connection.cursor() as cursor:
+	                cursor.execute(query)
+	                results = dictfetchall(cursor)
+	        return results
+
+	def dictfetchall(self, cursor):
+	    "Return all rows from a cursor as a dict"
+	    columns = [col[0] for col in cursor.description]
+	    return [
+	        dict(zip(columns, row))
+	        for row in cursor.fetchall()
+	    ]
+	
 	def readData(self):
 		filters = self.filters
 		filters = dict(filters.iterlists())
