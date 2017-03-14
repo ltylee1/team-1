f6ccc2bb0e0e23a9e25296a2b3bb9da3a9df51ae
Vivek Chachcha <vivek@seekmi.com>
2017-03-13 23:16:37 -0700

    removing de

--- uw_dashboard/databaseReader.py
+++ uw_dashboard/databaseReader.py
@@ -5,7 +5,7 @@ def my_custom_sql(query, is_dict):
         	cursor.execute(query)
 		if is_dict == 1:
 	                results = dictfetchall(cursor)
-		else
+		else:
 			results = cursor.fetchall()
         return results
 
@@ -24,9 +24,9 @@ class DatabaseReader(models.Model):
 	def readData(self):
 		filters = self.filters
 		filters = dict(filters.iterlists())
-		query = "SELECT * FROM uw_dashboard_program AS p, uw_dashboard_target_population AS t, uw_dashboard_geo_focus_area AS gfa, uw_dashboard_donor_engagement AS de WHERE"
+		query = "SELECT * FROM uw_dashboard_program AS p, uw_dashboard_target_population AS t, uw_dashboard_geo_focus_area AS gfa WHERE"
 
-		query += " p.program_andar_number = t.program_andar_number_id AND p.program_andar_number = gfa.program_andar_number_id AND p.program_andar_number = de.program_andar_number_id AND"
+		query += " p.program_andar_number = t.program_andar_number_id AND p.program_andar_number = gfa.program_andar_number_id AND"
 
 		if 'funding_year' in filters.keys():
 			query += " ("
@@ -49,13 +49,6 @@ class DatabaseReader(models.Model):
 			query = query[:-2]
 			query += ") AND"
 
-		if 'program_elements' in filters.keys():
-			query += " ("
-			for i in range(len(filters['program_elements'])):
-				query += " pe.specific_element = '" + str(filters['program_elements'][i]) + "' OR"
-			query = query[:-2]
-			query += ") AND"
-
 		if 'gfa' in filters.keys():
 			query += " ("
 			for i in range(len(filters['gfa'])):
@@ -70,13 +63,6 @@ class DatabaseReader(models.Model):
 	                query = query[:-2]
 	                query += ") AND"	
 	
-		if 'donor' in filters.keys():
-			query += " ("
-			for i in range(len(filters['donor'])):
-				query += " de.donor_engagement = '" + str(filters['donor'][i]) + "' OR"
-			query = query[:-2]
-			query += ") AND"
-
 		if 'money_invested' in filters.keys():
 			query += " ("
 			for i in filters['money_invested']:
@@ -100,9 +86,11 @@ class DatabaseReader(models.Model):
 		for program in programsReturned:
 			peQuery = "SELECT specific_element FROM uw_dashboard_program_elements WHERE program_andar_number_id = '" + str(program) + "'"
 			locQuery = "SELECT COUNT(id) AS total, postal_code FROM uw_dashboard_location WHERE program_andar_number_id = '" + str(program) + "'"
+			deQuery = "SELECT donor_engagement FROM uw_dashboard_donor_engagement WHERE program_andar_number_id = '" str(program) + "'"
 			programElem = (item for item in firstResults if item["program_andar_number"] == str(program)).next()
 			programElem['specific_elements'] = my_custom_sql(peQuery, 0)
 			programElem['locations'] = my_custom_sql(locQuery, 1)
+			programElemt['donor_engagement'] = my_custom_sql(deQuery, 0)
 
 		tQuery = "SELECT SUM(p.funds) AS invested, COUNT(p.program_andar_number) AS programs, COUNT(DISTINCT p.agency_andar_number_id) AS agencies, SUM(t.early_years) AS early_years, SUM(t.middle_years) AS middle_years, SUM(t.seniors) AS seniors, SUM(t.parent_caregivers) AS parent_caregivers, SUM(t.families) AS families, SUM(t.meals_snacks) as meals_snacks, SUM(t.counselling_sessions) AS counselling_sessions, SUM(t.mentors_tutors) AS mentors_tutors, SUM(t.workshops) as workshops, SUM(t.volunteers) AS volunteers FROM uw_dashboard_program as p, uw_dashboard_totals as t WHERE"
 
