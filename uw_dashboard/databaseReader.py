3acd4bea244c757603de448559c0ed613c4b78d3
Vivek Chachcha <vivek@seekmi.com>
2017-03-13 23:37:11 -0700

    group concat

--- uw_dashboard/databaseReader.py
+++ uw_dashboard/databaseReader.py
@@ -21,7 +21,9 @@ class DatabaseReader(models.Model):
 	def readData(self):
 		filters = self.filters
 		filters = dict(filters.iterlists())
-		query = "SELECT * FROM uw_dashboard_program AS p, uw_dashboard_program_elements AS pe, uw_dashboard_target_population AS t, uw_dashboard_geo_focus_area AS gfa, uw_dashboard_donor_engagement AS de WHERE"
+		query = "SELECT p.*, GROUP_CONCAT(pe.specific_element SEPARATOR ','), GROUP_CONCAT(de.donor_engagement SEPARATOR ','), GROUP_CONCAT(t.target_population SEPARATOR ',')"
+		
+		query += " FROM uw_dashboard_program AS p, uw_dashboard_program_elements AS pe, uw_dashboard_target_population AS t, uw_dashboard_geo_focus_area AS gfa, uw_dashboard_donor_engagement AS de WHERE"
 
 		query += " p.program_andar_number = pe.program_andar_number_id AND p.program_andar_number = t.program_andar_number_id AND p.program_andar_number = gfa.program_andar_number_id AND p.program_andar_number = de.program_andar_number_id AND"
 
@@ -88,7 +90,7 @@ class DatabaseReader(models.Model):
 			query = query[:-2]
 			query += ") AND"
 
-		query += " TRUE"
+		query += " TRUE GROUP BY p.program_andar_number, pe.program_andar_number_id, de.program_andar_number_id"
 
 		firstResults = my_custom_sql(query)
 		
