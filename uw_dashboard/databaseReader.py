92e03fbb33fb625750ffde8bf8629a37595d0dad
Vivek Chachcha <vivek@seekmi.com>
2017-03-13 21:07:27 -0700

    Updated queries to return more info

--- uw_dashboard/databaseReader.py
+++ uw_dashboard/databaseReader.py
@@ -1,9 +1,12 @@
 from django.db import connection, models
 
-def my_custom_sql(query):
+def my_custom_sql(query, is_dict):
 	with connection.cursor() as cursor:
         	cursor.execute(query)
-                results = dictfetchall(cursor)
+		if is_dict == 1:
+	                results = dictfetchall(cursor)
+		else
+			results = cursor.fetchall()
         return results
 
 def dictfetchall(cursor):
@@ -21,9 +24,9 @@ class DatabaseReader(models.Model):
 	def readData(self):
 		filters = self.filters
 		filters = dict(filters.iterlists())
-		query = "SELECT * FROM uw_dashboard_program AS p, uw_dashboard_program_elements AS pe, uw_dashboard_target_population AS t, uw_dashboard_geo_focus_area AS gfa, uw_dashboard_donor_engagement AS de WHERE"
+		query = "SELECT * FROM uw_dashboard_program AS p, uw_dashboard_target_population AS t, uw_dashboard_geo_focus_area AS gfa, uw_dashboard_donor_engagement AS de WHERE"
 
-		query += " p.program_andar_number = pe.program_andar_number_id AND p.program_andar_number = t.program_andar_number_id AND p.program_andar_number = gfa.program_andar_number_id AND p.program_andar_number = de.program_andar_number_id AND"
+		query += " p.program_andar_number = t.program_andar_number_id AND p.program_andar_number = gfa.program_andar_number_id AND p.program_andar_number = de.program_andar_number_id AND"
 
 		if 'funding_year' in filters.keys():
 			query += " ("
@@ -90,10 +93,16 @@ class DatabaseReader(models.Model):
 
 		query += " TRUE"
 
-		firstResults = my_custom_sql(query)
+		firstResults = my_custom_sql(query, 1)
 		
 		programsReturned = [i['program_andar_number'] for i in firstResults]
-		programsReturned = list(set(programsReturned))
+
+		for program in programsReturned:
+			peQuery = "SELECT specific_element FROM uw_dashboard_program_elements WHERE program_andar_number_id = '" + str(program) + "'"
+			locQuery = "SELECT COUNT(id) AS total, postal_code FROM uw_dashboard_location WHERE program_andar_number_id = '" + str(program) + "'"
+			programElem = (item for item in firstResults if item["program_andar_number"] == str(program)).next()
+			programElem['specific_elements'] = my_custom_sql(peQuery, 0)
+			programElem['locations'] = my_custom_sql(locQuery, 1)
 
 		tQuery = "SELECT SUM(p.funds) AS invested, COUNT(p.program_andar_number) AS programs, COUNT(DISTINCT p.agency_andar_number_id) AS agencies, SUM(t.early_years) AS early_years, SUM(t.middle_years) AS middle_years, SUM(t.seniors) AS seniors, SUM(t.parent_caregivers) AS parent_caregivers, SUM(t.families) AS families, SUM(t.meals_snacks) as meals_snacks, SUM(t.counselling_sessions) AS counselling_sessions, SUM(t.mentors_tutors) AS mentors_tutors, SUM(t.workshops) as workshops, SUM(t.volunteers) AS volunteers FROM uw_dashboard_program as p, uw_dashboard_totals as t WHERE"
 
@@ -107,6 +116,6 @@ class DatabaseReader(models.Model):
 			tQuery += ") AND"
 		tQuery += " TRUE"
 
-		tResults = my_custom_sql(tQuery)
+		tResults = my_custom_sql(tQuery, 1)
 
 		return {'results': firstResults, 'totals': tResults, 'query': query, 'tquery': tQuery, 'filters': filters}	
