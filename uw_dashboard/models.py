9084960fc423cde13b0ebde0fff5b361907e1586
Ty <lulu.tylee@gmail.com>
2017-03-25 15:11:28 -0700

    Updating map location queries

--- uw_dashboard/models.py
+++ uw_dashboard/models.py
@@ -151,36 +151,29 @@ class Reporting_Service:
         results = dbReader.readData()
         return results
 
-    def queryMap(self):
+    def queryMap(self, postal_codes):
         location_name = []
         program_names = []
         location_lat = []
         location_lon = []
         addresses = []
-        locations = Location.objects.all()
-        for location in locations:
-            location_name.append(str(location.location))
-            program_names.append(str(location.program_name))
-            location_lat.append(str(location.latitude))
-            location_lon.append(str(location.longitude))
-            addresses.append(str(location.address))
+        if postal_codes:
+            for loc_post in postal_codes:
+                location = Location.objects.filter(postal_code=loc_post).get()
+                location_name.append(str(location.location))
+                program_names.append(str(location.program_name))
+                location_lat.append(str(location.latitude))
+                location_lon.append(str(location.longitude))
+                addresses.append(str(location.address))
+        else:
+            locations = Location.objects.all()
+            for location in locations:
+                location_name.append(str(location.location))
+                program_names.append(str(location.program_name))
+                location_lat.append(str(location.latitude))
+                location_lon.append(str(location.longitude))
+                addresses.append(str(location.address))
         return [addresses, location_name, program_names, location_lat, location_lon]
-
-    def queryMapPostal(self, postal_codes):
-        location_name = []
-        program_names = []
-        location_lat = []
-        location_lon = []
-        addresses = []
-        for loc_post in postal_codes:
-            location = Location.objects.filter(postal_code=loc_post).get()
-            location_name.append(str(location.location))
-            program_names.append(str(location.program_name))
-            location_lat.append(str(location.latitude))
-            location_lon.append(str(location.longitude))
-            addresses.append(str(location.address))
-        return [addresses, location_name, program_names, location_lat, location_lon]
-
 
     def create_dashboard(self):
         # call the dashboard generator
