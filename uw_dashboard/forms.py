d85383957b2d98daa0a579d6802ec9e2fcf13c62
Richard Chiang <shadowlairs@gmail.com>
2017-03-26 20:11:17 -0700

    Styled the upload form

--- uw_dashboard/forms.py
+++ uw_dashboard/forms.py
@@ -10,14 +10,14 @@ YEARS = (
     (2017, '2017/2018'),
 )
 
-TYPE = (
+TYPE = [
     ('output', 'Inventory and Outputs'),
     ('postal', 'Program Locations'),
-)
+]
 
 # widget=forms.TextInput(attrs={'class': 'col-md-4'})
 class UploadFileForm(forms.Form):
-    File_To_Upload = forms.FileField()
+    File_To_Upload = forms.FileField(widget=forms.ClearableFileInput(attrs= {'class': 'upload_btn'}))
     Overwrite_data = forms.BooleanField(required=False)
     File_type = forms.ChoiceField(TYPE)
     Funding_Year = forms.ChoiceField(YEARS)
